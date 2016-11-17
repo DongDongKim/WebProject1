cee8168c136aa7ba6d00524e7040074fdcad3a78
DongDongKim <cjdtkddkrrl@daum.net>
2016-11-17 13:01:51 +0900

    modal

--- airbnd/app.js
+++ airbnd/app.js
@@ -28,7 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
